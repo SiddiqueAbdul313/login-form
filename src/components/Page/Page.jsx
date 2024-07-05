a28f598c2db1e2de5662ca96cc07a4ca9d4b25b3
Siddique Abdul <szuber558@gmail.com>
2024-07-05 16:07:49 +0530

    demo

---
+++ src/components/Page/Page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Page.css";
+import "../../font.css";
+import { Typography } from '@mui/material';
+
+function Page() {
+  return (
+    <div>
+        <Typography className='title' sx={{fontFamily:'r-bold'}}>Hello</Typography>
+    </div>
+  )
+}
+
+export default Page
